f0c45068ef619e9b6cdc31e269dd0d8c10e93e29
Pasoi-Stefan <pasoi0stefan@gmail.com>
2023-05-23 21:08:10 +0300

    use smaller samples of train and dev

--- data_utils/data_utils.py
+++ data_utils/data_utils.py
@@ -2,22 +2,29 @@ from .text_data_processors import get_data as get_text_data
 from .image_data_processors import get_data as get_image_data
 
 
+import random
 import pandas as pd
 from .text_data_processors import InputExample
 
-def get_covid19stance_data():
+def get_covid19stance_data(train_num_per_class, dev_num_per_class):
     examples = {
         "train" : [],
         "dev": [],
         "test": []
     }
-
+ 
     train_examples = pd.read_csv("datasets/covid-19-stance/raw_train_all_onecol.csv")
     dev_examples = pd.read_csv("datasets/covid-19-stance/raw_val_all_onecol.csv")
     test_examples = pd.read_csv("datasets/covid-19-stance/raw_test_all_onecol.csv")
 
     examples["train"] = [InputExample(f"train-{i}", row["Tweet"], None, row["Stance 1"]) for i, row in enumerate (train_examples.iloc)]
+    random.shuffle(examples["train"])
+    examples["train"] = examples["train"][:train_num_per_class]
+
     examples["dev"] = [InputExample(f"dev-{i}", row["Tweet"], None, row["Stance 1"]) for i, row in enumerate (dev_examples.iloc)]
+    random.shuffle(examples["dev"])
+    examples["dev"] = examples["dev"][:dev_num_per_class]
+
     examples["test"] = [InputExample(f"test-{i}", row["Tweet"], None, row["Stance 1"]) for i, row in enumerate (test_examples.iloc)]
 
     return examples, ["FAVOR", "AGAINST", "NONE"]
@@ -26,7 +33,7 @@ def get_covid19stance_data():
 def get_data(task, train_num_per_class, dev_num_per_class, imbalance_rate,
              data_seed):
     if task in ['covid-19-stance']:
-        return get_covid19stance_data()
+        return get_covid19stance_data(train_num_per_class, dev_num_per_class)
 
     if task in ['sst-2', 'sst-5']:
         return get_text_data(
